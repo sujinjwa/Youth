2a08213eee26383047ac0adfc6716cf06f07bec9
sujinjwa <91577550+sujinjwa@users.noreply.github.com>
2022-10-15 22:34:15 +0900

    CORS policy 에러 문제 해결 중

--- src/middlewares.js
+++ src/middlewares.js
@@ -4,6 +4,8 @@ import multerS3 from "multer-s3";
 
 // 모든 템플릿에서 사용 가능한 전역 변수 선언
 export const localsMiddleware = (req, res, next) => {
+  res.setHeader("Access-Control-Allow-origin", "*");
+
   res.locals.loggedIn = Boolean(req.session.loggedIn);
   res.locals.loggedInUser = req.session.loggedInUser;
   res.locals.isHeroku = isHeroku;
