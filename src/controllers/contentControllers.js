bc7b63b4e28d660ee3440c2953213e28b4e7e63e
sujinjwa <91577550+sujinjwa@users.noreply.github.com>
2022-11-03 23:39:15 +0900

    feat: QR랜딩 페이지의 각 콘텐츠 별로 데이터 전송

--- src/controllers/contentControllers.js
+++ src/controllers/contentControllers.js
@@ -34,9 +34,22 @@ export const detail = (req, res) => {
   return res.render("contents/detail", { pageTitle: "QR코드 렌더링" });
 };
 
+const qrContents = [
+  { title: "유골을 뿌리는 장례 방식(산분장) 더 알아보기", hashtags: "장례" },
+  { title: "장기기증 더 알아보기", hashtags: "장례 • 장기기증" },
+  { title: "상속포기와 한정승인 더 알아보기", hashtags: "상속 • 재산" },
+  { title: "SNS 기념(추모)계정 더 알아보기", hashtags: "장례" },
+  { title: "유품 소각 관련 사항 더 알아보기", hashtags: "재산" },
+  { title: "반려동물 신탁제도 더 알아보기", hashtags: "장례 • 재산" },
+];
+
 export const qrPage = (req, res) => {
   const id = req.params.id;
-  return res.send(`page ${id}`);
+  return res.render("contents/qrpage", {
+    pageTitle: "Contents",
+    title: qrContents[id - 1].title,
+    imgUrl: `/uploads/contents/${id}.jpg`,
+  });
 };
 
 export const recommend = (req, res) => {
