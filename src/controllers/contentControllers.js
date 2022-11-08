92302e91257b7984af526ab4af16560248504b81
sujinjwa <91577550+sujinjwa@users.noreply.github.com>
2022-11-08 15:10:18 +0900

    style: 전체적인 용어 변경

--- src/controllers/contentControllers.js
+++ src/controllers/contentControllers.js
@@ -1,64 +1,67 @@
-import express from "express";
+import express from 'express';
 
 export const home = (req, res) => {
   const popup = req.query.popup;
-  return res.render("home", {
-    pageTitle: "Home",
+  return res.render('home', {
+    pageTitle: 'Home',
     popup,
   });
 };
 
 export const about = (req, res) => {
-  return res.render("contents/about", { pageTitle: "About" });
+  return res.render('contents/about', { pageTitle: 'About' });
 };
 
 export const youthkit = (req, res) => {
-  return res.render("contents/youthkit");
+  return res.render('contents/youthkit');
 };
 
 export const community = (req, res) => {
   const popup = encodeURIComponent(
-    "현재 커뮤니티 서비스 개발 중입니다. \n조금만 기다려주세요."
+    '현재 커뮤니티 서비스 개발 중입니다. \n조금만 기다려주세요.'
   );
-  return res.redirect("/?popup=" + popup);
+  return res.redirect('/?popup=' + popup);
 };
 
 export const qna = (req, res) => {
   const popup = req.query.popup;
-  // console.log(req.params);
   let id = req.params.id;
-  return res.render(`contents/qna_${id}`, { pageTitle: "QnA", popup });
+  return res.render(`contents/qna_${id}`, { pageTitle: 'QnA', popup });
 };
 
 export const detail = (req, res) => {
-  return res.render("contents/detail", { pageTitle: "QR코드 렌더링" });
+  return res.render('contents/detail', { pageTitle: '가이드북 플러스' });
 };
 
 const qrContents = [
   {
-    title: "유골을 뿌리는 장례 방식(산분장) 더 알아보기",
-    hashtags: "장례",
+    title: '유골을 뿌리는 장례 방식',
+    hashtags: '장례 • 장례방식',
     minutes: 3,
   },
-  { title: "장기기증 더 알아보기", hashtags: "장례 • 장기기증", minutes: 2 },
+  { title: '상황별 기증 가능 장기 종류', hashtags: '장기기증', minutes: 2 },
   {
-    title: "상속포기와 한정승인 더 알아보기",
-    hashtags: "상속 • 재산",
+    title: '부채와 상속',
+    hashtags: '상속포기 • 빚 • 재산',
     minutes: 2,
   },
-  { title: "SNS 기념(추모)계정 더 알아보기", hashtags: "장례", minutes: 4 },
-  { title: "유품 소각 관련 사항 더 알아보기", hashtags: "재산", minutes: 3 },
   {
-    title: "반려동물 신탁제도 더 알아보기",
-    hashtags: "장례 • 재산",
+    title: 'SNS 기념(추모)계정 전환방법',
+    hashtags: '추모 • 디지털 기록',
+    minutes: 4,
+  },
+  { title: '유품 처분 방법', hashtags: '유품 소각 • 유품 정리', minutes: 3 },
+  {
+    title: '반려동물 신탁제도(펫신탁)',
+    hashtags: '반려동물 • 신탁 • 대리인',
     minutes: 2,
   },
 ];
 
 export const qrPage = (req, res) => {
   const id = req.params.id;
-  return res.render("contents/qrpage", {
-    pageTitle: "Contents",
+  return res.render('contents/qrpage', {
+    pageTitle: 'Contents',
     title: qrContents[id - 1].title,
     imgUrl: `/uploads/contents/${id}.jpg`,
   });
@@ -66,7 +69,7 @@ export const qrPage = (req, res) => {
 
 export const recommend = (req, res) => {
   const popup = encodeURIComponent(
-    "현재 추천콘텐츠 서비스 개발 중입니다. \n조금만 기다려주세요."
+    '현재 추천콘텐츠 서비스 개발 중입니다. \n조금만 기다려주세요.'
   );
-  return res.redirect("/contents/qna/1?popup=" + popup);
+  return res.redirect('/contents/qna/1?popup=' + popup);
 };
