d4d32be4b40181e9434a82d2d24dc3bde7ddb4f3
sujinjwa <91577550+sujinjwa@users.noreply.github.com>
2022-11-10 10:41:12 +0900

    refactor: qrContents 배열을 독립적인 파일로 분리

--- src/controllers/contentControllers.js
+++ src/controllers/contentControllers.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import { qrContents } from './qrContents';
 
 export const home = (req, res) => {
   const popup = req.query.popup;
@@ -32,31 +33,6 @@ export const qna = (req, res) => {
 export const detail = (req, res) => {
   return res.render('contents/detail', { pageTitle: '가이드북 플러스' });
 };
-
-const qrContents = [
-  {
-    title: '유골을 뿌리는 장례 방식',
-    hashtags: '장례 • 장례방식',
-    minutes: 3,
-  },
-  { title: '상황별 기증 가능 장기 종류', hashtags: '장기기증', minutes: 2 },
-  {
-    title: '부채와 상속',
-    hashtags: '상속포기 • 빚 • 재산',
-    minutes: 2,
-  },
-  {
-    title: 'SNS 기념(추모)계정 전환방법',
-    hashtags: '추모 • 디지털 기록',
-    minutes: 4,
-  },
-  { title: '유품 처분 방법', hashtags: '유품 소각 • 유품 정리', minutes: 3 },
-  {
-    title: '반려동물 신탁제도(펫신탁)',
-    hashtags: '반려동물 • 신탁 • 대리인',
-    minutes: 2,
-  },
-];
 
 export const qrPage = (req, res) => {
   const id = req.params.id;
---
+++ src/controllers/qrContents.js
@@ -0,0 +1,24 @@
+export const qrContents = [
+  {
+    title: '유골을 뿌리는 장례 방식',
+    hashtags: '장례 • 장례방식',
+    minutes: 3,
+  },
+  { title: '상황별 기증 가능 장기 종류', hashtags: '장기기증', minutes: 2 },
+  {
+    title: '부채와 상속',
+    hashtags: '상속포기 • 빚 • 재산',
+    minutes: 2,
+  },
+  {
+    title: 'SNS 기념(추모)계정 전환방법',
+    hashtags: '추모 • 디지털 기록',
+    minutes: 4,
+  },
+  { title: '유품 처분 방법', hashtags: '유품 소각 • 유품 정리', minutes: 3 },
+  {
+    title: '반려동물 신탁제도(펫신탁)',
+    hashtags: '반려동물 • 신탁 • 대리인',
+    minutes: 2,
+  },
+];
