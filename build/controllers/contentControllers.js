021bf63967ec8d4d32b81c96ee1921defa62838d
sujinjwa <91577550+sujinjwa@users.noreply.github.com>
2022-11-12 23:59:24 +0900

    build: 빌드 완료

--- build/controllers/contentControllers.js
+++ build/controllers/contentControllers.js
@@ -7,6 +7,8 @@ exports.youthkit = exports.recommend = exports.qrPage = exports.qna = exports.ho
 
 var _express = _interopRequireDefault(require("express"));
 
+var _qrContents = require("./qrContents");
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
 var home = function home(req, res) {
@@ -58,37 +60,12 @@ var detail = function detail(req, res) {
 };
 
 exports.detail = detail;
-var qrContents = [{
-  title: '유골을 뿌리는 장례 방식',
-  hashtags: '장례 • 장례방식',
-  minutes: 3
-}, {
-  title: '상황별 기증 가능 장기 종류',
-  hashtags: '장기기증',
-  minutes: 2
-}, {
-  title: '부채와 상속',
-  hashtags: '상속포기 • 빚 • 재산',
-  minutes: 2
-}, {
-  title: 'SNS 기념(추모)계정 전환방법',
-  hashtags: '추모 • 디지털 기록',
-  minutes: 4
-}, {
-  title: '유품 처분 방법',
-  hashtags: '유품 소각 • 유품 정리',
-  minutes: 3
-}, {
-  title: '반려동물 신탁제도(펫신탁)',
-  hashtags: '반려동물 • 신탁 • 대리인',
-  minutes: 2
-}];
 
 var qrPage = function qrPage(req, res) {
   var id = req.params.id;
-  return res.render('contents/qrpage', {
+  return res.render("contents/qrpage".concat(id), {
     pageTitle: 'Contents',
-    title: qrContents[id - 1].title,
+    title: _qrContents.qrContents[id - 1].title,
     imgUrl: "/uploads/contents/".concat(id, ".jpg")
   });
 };
