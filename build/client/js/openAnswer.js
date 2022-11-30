12b7fa9de2795789705db2851bb4d8b2ca7d9f5a
sujinjwa <91577550+sujinjwa@users.noreply.github.com>
2022-11-30 17:48:44 +0900

    build: 수정 사항 빌드 완료

--- build/client/js/openAnswer.js
+++ build/client/js/openAnswer.js
@@ -1,27 +1,28 @@
 "use strict";
 
-var heading = document.querySelectorAll(".panel-heading");
-var answer = document.querySelectorAll(".panel-body");
-var question = document.querySelectorAll(".panel-question");
+var heading = document.querySelectorAll('.panel-heading');
+var answer = document.querySelectorAll('.panel-body');
+var question = document.querySelectorAll('.panel-question');
 
 var openAnswer = function openAnswer(event) {
   for (var i = 0; i < question.length; i++) {
     var q = event.target.parentNode;
 
     if (q == question[i] || q.parentNode == question[i] || q.parentNode.parentNode == question[i]) {
-      question[i].classList.toggle("clicked");
+      question[i].classList.toggle('clicked');
     }
 
-    if (question[i].classList.contains("clicked")) {
-      var icon = question[i].querySelector("div>div>h3>i");
-      icon.className = "fa-solid fa-angle-up";
+    if (question[i].classList.contains('clicked')) {
+      var icon = question[i].querySelector('div>div>h3>i');
+      icon.className = 'fa-solid fa-angle-up';
     } else {
-      var icon = question[i].querySelector("div>div>h3>i");
-      icon.className = "fa-solid fa-angle-down";
+      var _icon = question[i].querySelector('div>div>h3>i');
+
+      _icon.className = 'fa-solid fa-angle-down';
     }
   }
 };
 
 for (var i = 0; i < heading.length; i++) {
-  heading[i].addEventListener("click", openAnswer);
+  heading[i].addEventListener('click', openAnswer);
 }
--- build/client/js/toggleBtn.js
+++ build/client/js/toggleBtn.js
@@ -1,22 +1,22 @@
 "use strict";
 
-var toggle = document.querySelector(".toggle");
-var indicator = document.querySelector(".indicator");
-var span = document.querySelectorAll(".panel-body p span");
-var h3 = document.querySelector(".container__toggle h3");
+var toggle = document.querySelector('.toggle');
+var indicator = document.querySelector('.indicator');
+var span = document.querySelectorAll('.panel-body .answer span');
+var h3 = document.querySelector('.container__toggle h3');
 
 var clickToggle = function clickToggle() {
-  toggle.classList.toggle("active");
+  toggle.classList.toggle('active');
 
-  if (toggle.classList.contains("active")) {
-    h3.innerText = "일상 용어 모드";
+  if (toggle.classList.contains('active')) {
+    h3.innerText = '일상 용어 모드';
   } else {
-    h3.innerText = "법률 용어 모드";
+    h3.innerText = '법률 용어 모드';
   }
 
   for (var i = 0; i < span.length; i++) {
-    span[i].classList.toggle("toggle");
+    span[i].classList.toggle('toggle');
   }
 };
 
-toggle.addEventListener("click", clickToggle);
+toggle.addEventListener('click', clickToggle);
