76a454abc9458ed3503d28ff51c4c2d5cefdc2b9
sujinjwa <91577550+sujinjwa@users.noreply.github.com>
2022-11-30 17:47:07 +0900

    feat: QnA 내용 채우기

--- src/client/js/openAnswer.js
+++ src/client/js/openAnswer.js
@@ -1,28 +1,28 @@
-const heading = document.querySelectorAll(".panel-heading");
-const answer = document.querySelectorAll(".panel-body");
-const question = document.querySelectorAll(".panel-question");
+const heading = document.querySelectorAll('.panel-heading');
+const answer = document.querySelectorAll('.panel-body');
+const question = document.querySelectorAll('.panel-question');
 
 const openAnswer = (event) => {
-  for (var i = 0; i < question.length; i++) {
-    var q = event.target.parentNode;
+  for (let i = 0; i < question.length; i++) {
+    let q = event.target.parentNode;
     if (
       q == question[i] ||
       q.parentNode == question[i] ||
       q.parentNode.parentNode == question[i]
     ) {
-      question[i].classList.toggle("clicked");
+      question[i].classList.toggle('clicked');
     }
 
-    if (question[i].classList.contains("clicked")) {
-      var icon = question[i].querySelector("div>div>h3>i");
-      icon.className = "fa-solid fa-angle-up";
+    if (question[i].classList.contains('clicked')) {
+      let icon = question[i].querySelector('div>div>h3>i');
+      icon.className = 'fa-solid fa-angle-up';
     } else {
-      var icon = question[i].querySelector("div>div>h3>i");
-      icon.className = "fa-solid fa-angle-down";
+      let icon = question[i].querySelector('div>div>h3>i');
+      icon.className = 'fa-solid fa-angle-down';
     }
   }
 };
 
-for (var i = 0; i < heading.length; i++) {
-  heading[i].addEventListener("click", openAnswer);
+for (let i = 0; i < heading.length; i++) {
+  heading[i].addEventListener('click', openAnswer);
 }
--- src/client/js/toggleBtn.js
+++ src/client/js/toggleBtn.js
@@ -1,19 +1,19 @@
-const toggle = document.querySelector(".toggle");
-const indicator = document.querySelector(".indicator");
-const span = document.querySelectorAll(".panel-body p span");
-const h3 = document.querySelector(".container__toggle h3");
+const toggle = document.querySelector('.toggle');
+const indicator = document.querySelector('.indicator');
+const span = document.querySelectorAll('.panel-body .answer span');
+const h3 = document.querySelector('.container__toggle h3');
 
 const clickToggle = () => {
-  toggle.classList.toggle("active");
+  toggle.classList.toggle('active');
 
-  if (toggle.classList.contains("active")) {
-    h3.innerText = "일상 용어 모드";
+  if (toggle.classList.contains('active')) {
+    h3.innerText = '일상 용어 모드';
   } else {
-    h3.innerText = "법률 용어 모드";
+    h3.innerText = '법률 용어 모드';
   }
   for (var i = 0; i < span.length; i++) {
-    span[i].classList.toggle("toggle");
+    span[i].classList.toggle('toggle');
   }
 };
 
-toggle.addEventListener("click", clickToggle);
+toggle.addEventListener('click', clickToggle);
