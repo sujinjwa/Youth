b0fc5cfe2e4b9b4b856e57d93eccc49b89bd8dff
sujinjwa <91577550+sujinjwa@users.noreply.github.com>
2022-12-19 16:52:00 +0900

    회원가입 전 개인 정보 동의 페이지 관련 라우터,컨트롤러 생성 및 UI 제작하여 반응형 style 적용

--- build/routers/globalRouters.js
+++ build/routers/globalRouters.js
@@ -17,21 +17,21 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "d
 
 var globalRouter = _express["default"].Router();
 
-globalRouter.get("/", _contentControllers.home);
-globalRouter.get("/about", _contentControllers.about);
-globalRouter.get("/youthkit", _contentControllers.youthkit);
-globalRouter.get("/community", _middlewares.protectorMiddleware, _contentControllers.community);
-globalRouter.route("/join").all(_middlewares.publicOnlyMiddleware).get(_userControllers.getJoin).post(_userControllers.postJoin);
-globalRouter.get("/welcome", _userControllers.welcome);
-globalRouter.route("/login").all(_middlewares.publicOnlyMiddleware).get(_userControllers.getLogin).post(_userControllers.postLogin);
-globalRouter.get("/login/kakao/start", _middlewares.publicOnlyMiddleware, _userControllers.startKakaoLogin);
-globalRouter.get("/login/kakao/finish", _middlewares.publicOnlyMiddleware, _userControllers.finishKakaoLogin);
-globalRouter.get("/login/naver/start", _middlewares.publicOnlyMiddleware, _userControllers.startNaverLogin);
-globalRouter.get("/login/naver/finish", _middlewares.publicOnlyMiddleware, _userControllers.finishNaverLogin);
-globalRouter.get("/logout", _middlewares.protectorMiddleware, _userControllers.logout);
-globalRouter.route("/login/findID").all(_middlewares.publicOnlyMiddleware).get(_userControllers.getFindID).post(_userControllers.postFindID);
-globalRouter.get("/login/showID", _middlewares.publicOnlyMiddleware, _userControllers.showID);
-globalRouter.route("/login/findPW").all(_middlewares.publicOnlyMiddleware).get(_userControllers.getFindPW).post(_userControllers.postFindPW); // globalRouter.get("/login/findPW", findPW);
+globalRouter.get('/', _contentControllers.home);
+globalRouter.get('/about', _contentControllers.about);
+globalRouter.get('/youthkit', _contentControllers.youthkit);
+globalRouter.get('/community', _middlewares.protectorMiddleware, _contentControllers.community);
+globalRouter.route('/join').all(_middlewares.publicOnlyMiddleware).get(_userControllers.getJoin).post(_userControllers.postJoin);
+globalRouter.get('/welcome', _userControllers.welcome);
+globalRouter.route('/login').all(_middlewares.publicOnlyMiddleware).get(_userControllers.getLogin).post(_userControllers.postLogin);
+globalRouter.get('/login/kakao/start', _middlewares.publicOnlyMiddleware, _userControllers.startKakaoLogin);
+globalRouter.get('/login/kakao/finish', _middlewares.publicOnlyMiddleware, _userControllers.finishKakaoLogin);
+globalRouter.get('/login/naver/start', _middlewares.publicOnlyMiddleware, _userControllers.startNaverLogin);
+globalRouter.get('/login/naver/finish', _middlewares.publicOnlyMiddleware, _userControllers.finishNaverLogin);
+globalRouter.get('/logout', _middlewares.protectorMiddleware, _userControllers.logout);
+globalRouter.route('/login/findID').all(_middlewares.publicOnlyMiddleware).get(_userControllers.getFindID).post(_userControllers.postFindID);
+globalRouter.get('/login/showID', _middlewares.publicOnlyMiddleware, _userControllers.showID);
+globalRouter.route('/login/findPW').all(_middlewares.publicOnlyMiddleware).get(_userControllers.getFindPW).post(_userControllers.postFindPW); // globalRouter.get("/login/findPW", findPW);
 
 var _default = globalRouter;
 exports["default"] = _default;
