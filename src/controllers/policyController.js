d2a135766f933fd75be5a82e7eed88ce15fa941a
sujinjwa <91577550+sujinjwa@users.noreply.github.com>
2022-12-22 15:10:05 +0900

    feat: 개인정보 이용약관 내용 추가

--- src/controllers/policyController.js
+++ src/controllers/policyController.js
@@ -1,12 +1,14 @@
 import express from 'express';
-import { Policy1 } from '../data/policy';
+import { Policy1, Policy2 } from '../data/policy';
 
 export const getPolicy = (req, res) => {
-  const POLICY = Policy1.contents.split('\n');
+  const POLICY1 = Policy1.contents.split('\n');
+  const POLICY2 = Policy2.contents.split('\n');
 
   return res.render('policy/policy', {
     pageTitle: '이용약관',
-    contents: POLICY,
+    contents1: POLICY1,
+    contents2: POLICY2,
   });
 };
 
